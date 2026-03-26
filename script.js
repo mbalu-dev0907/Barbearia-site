0505251cd3a8a755755be7ddcb960972e45f9f3c
mbalu-dev0907 <sernandombalu@outlook.com>
2026-03-26 08:53:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function mostrarMensagem() {
-  alert("Agendamento em breve disponível!");
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
 }
